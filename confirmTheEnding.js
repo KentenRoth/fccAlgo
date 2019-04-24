fcf30b19bc799944e8a7c38f893f17d1151fc00d
Kent Roth <kentencroth@yahoo.com>
2019-04-24 17:18:59 -0500

    Adding in 2 challenges for tomorrow

---
+++ confirmTheEnding.js
@@ -0,0 +1,9 @@
+function confirmEnding(str, target) {
+	return str;
+}
+
+confirmEnding('Bastian', 'n'); // should return true
+
+module.exports = confirmEnding;
+
+// Checks to see if the target matches the ending of the string
---
+++ repeatString.js
@@ -0,0 +1,9 @@
+function repeatStringNumTimes(str, num) {
+	return str;
+}
+
+repeatStringNumTimes('abc', 3); //should return abcabcabc
+
+module.exports = repeatStringNumTimes;
+
+// string should repeat itself by the num number of times
